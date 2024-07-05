6a0b68c269d4412741f9e7b444d6ef3d852d4fd1
LupoArticoblu <wolf94-@libero.it>
2024-07-05 16:43:36 +0200

    parte uno cli vite

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+
+  import HeaderComponent from './components/HEADER/HeaderComponent.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      HeaderComponent
+    },
+    //qui vanno anche i nostri vecchi amici: data, metods, mounted...
+    data() {
+      return {
+        msg: 'Ciao vue-vite da data'
+      }
+    },
+
+  }
+</script>
+
+<template>
+  <HeaderComponent/>
+  <h1>SALVI</h1>
+  <h2>{{ msg }}</h2>
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/HEADER/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'HeaderComponent',
+  data() {
+    return {
+      title: 'Ciao, sono il titolo del componente'
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>{{ title }}</h2>
+</template>
+
+<style scoped>
+  h2{
+    color: red;
+    font-size: 2.3 rem;
+  }
+</style>
